207c21e888f1c790c1e3336defcde1da872da360
lapisdecor <lapisdecor@gmail.com>
2017-03-04 17:45:05 +0000

    hellooo

--- bzoinq/bzoinq.py
+++ bzoinq/bzoinq.py
@@ -1,1 +1,11 @@
 # -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+def hello():
+    """Returns Heloooo"""
+    return ("Helooo")
+
+def say_hello():
+    """ Prints Hellooo message """
+    print(hello())
