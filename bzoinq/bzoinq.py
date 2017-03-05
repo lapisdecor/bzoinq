c975a6f5fe4162bcf5ffaf81ae9e5e1c20a4c864
lapisdecor <lapisdecor@gmail.com>
2017-03-05 00:15:51 +0000

    changed .yml

--- bzoinq/bzoinq.py
+++ bzoinq/bzoinq.py
@@ -2,13 +2,16 @@
 
 from __future__ import print_function
 
+
 def hello():
     """Returns Heloooo"""
     return ("Helooo")
 
+
 def say_hello():
     """ Prints Hellooo message """
     print(hello())
+
 
 def say_something_else():
     """prints something else"""
