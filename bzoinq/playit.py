bdf267149794c8b33e97b40bd2ca8e29ab19ed96
Luis Louro <lapisdecor@gmail.com>
2018-12-21 18:44:48 +0000

    Change pyaudio to paplay

--- bzoinq/playit.py
+++ bzoinq/playit.py
@@ -1,33 +1,10 @@
-import pyaudio
-import wave
-import time
+import subprocess
 from pkg_resources import resource_filename
 
 
 def playit(file):
-    """function used to play a sound file"""
-
+    """
+    Function used to play a sound file
+    """
     filepath = resource_filename(__name__, 'sound/' + file)
-    wf = wave.open(filepath, 'rb')
-    p = pyaudio.PyAudio()
-
-    def callback(in_data, frame_count, time_info, status):
-        data = wf.readframes(frame_count)
-        return (data, pyaudio.paContinue)
-
-    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                    channels=wf.getnchannels(),
-                    rate=wf.getframerate(),
-                    output=True,
-                    stream_callback=callback)
-
-    stream.start_stream()
-
-    while stream.is_active():
-        time.sleep(0.1)
-
-    stream.stop_stream()
-    stream.close()
-    wf.close()
-
-    p.terminate()
+    subprocess.Popen(["paplay", filepath])
